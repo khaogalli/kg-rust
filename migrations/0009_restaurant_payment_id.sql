b77edb18bec4aec3493f70dfed4f8e4b830e1427
Amal <amalb11164@gmail.com>
2024-07-24 12:01:55 +0530

    payments

---
+++ migrations/0009_restaurant_payment_id.sql
@@ -0,0 +1,2 @@
+alter table "restaurant" add cashfree_app_id text;
+alter table "restaurant" add cashfree_secret_key text;
--- src/api/orders.rs
+++ src/api/orders.rs
@@ -1,7 +1,12 @@
+use anyhow::Context;
 use axum::extract::{Path, State};
+use axum::response::IntoResponse;
 use axum::routing::{get, post};
 use axum::{Json, Router};
 use chrono::Local;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 
 use crate::api::auth::{Auth, AuthRestaurant, AuthUser};
 use crate::api::restaurants::get_restaurant_name;
@@ -15,6 +20,8 @@ pub(crate) fn router() -> Router<AppContext> {
         .route("/api/orders/pending", get(get_pending_orders))
         .route("/api/orders/complete/:order_id", post(complete_order))
         .route("/api/orders/:days", get(get_orders))
+        .route("/api/orders/payment/:order_id", get(get_payment_session))
+        .route("/api/orders/payment/verify/:order_id", get(verify_payment))
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -23,7 +30,7 @@ struct OrderBody<T> {
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
-struct Order {
+pub(super) struct Order {
     id: uuid::Uuid,
     restaurant_id: uuid::Uuid,
     restaurant_name: String,
@@ -52,6 +59,13 @@ struct NewOrder {
 struct NewItem {
     id: uuid::Uuid,
     quantity: i32,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct PaymentResponse {
+    cf_order_id: String,
+    payment_session_id: String,
+    order_status: String,
 }
 
 async fn make_order(
@@ -98,6 +112,59 @@ async fn make_order(
         .await?;
     }
 
+    let rk = sqlx::query!(
+        r#"select cashfree_app_id, cashfree_secret_key from restaurant where restaurant_id=$1"#,
+        req.order.restaurant_id
+    )
+    .fetch_one(&mut *tx)
+    .await?;
+
+    let client = Client::new();
+
+    let cf_order_data = json!({
+        "order_amount": total,
+        "order_currency": "INR",
+        "customer_details": {
+            "customer_id": auth_user.user_id,
+            "customer_name": &get_username(auth_user.user_id, &ctx).await?,
+            "customer_phone": "+919999999999"
+        }
+    });
+
+    let response = client
+        .post("https://sandbox.cashfree.com/pg/orders")
+        .header(
+            "X-Client-Secret",
+            rk.cashfree_secret_key
+                .context("cashfree_secret_key is empyt")?,
+        )
+        .header(
+            "X-Client-Id",
+            rk.cashfree_app_id.context("cashfree_app_id is empty")?,
+        )
+        .header("x-api-version", "2023-08-01")
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
+        .json(&cf_order_data)
+        .send()
+        .await
+        .context("failed to create payment session")?;
+
+    if !response.status().is_success() {
+        return Err(anyhow::anyhow!("failed to create payment session").into());
+    }
+
+    let response: PaymentResponse = response.json().await.context("failed to parse response")?;
+
+    sqlx::query!(
+        r#"insert into "payment" (cf_order_id,order_id,payment_session_id) values ($1,$2,$3)"#,
+        response.cf_order_id,
+        order.order_id,
+        response.payment_session_id
+    )
+    .execute(&mut *tx)
+    .await?;
+
     tx.commit().await?;
 
     Ok(Json(OrderBody {
@@ -113,6 +180,95 @@ async fn make_order(
             created_at: order.created_at.with_timezone(&chrono::Local),
         },
     }))
+}
+
+#[derive(Deserialize)]
+struct PaymentStatus {
+    order_status: String,
+}
+
+async fn get_payment_session(
+    Path(order_id): Path<uuid::Uuid>,
+    ctx: State<AppContext>,
+) -> Result<Json<String>> {
+    let payment_session_id = sqlx::query!(
+        r#"select payment_session_id from "payment" where order_id = $1"#,
+        order_id
+    )
+    .fetch_one(&ctx.db)
+    .await?;
+
+    Ok(Json(payment_session_id.payment_session_id))
+}
+
+async fn verify_payment(
+    Path(order_id): Path<uuid::Uuid>,
+    ctx: State<AppContext>,
+) -> Result<impl IntoResponse> {
+    let cf_order_id = sqlx::query_scalar!(
+        r#"select cf_order_id from "payment" where order_id = $1"#,
+        order_id
+    )
+    .fetch_one(&ctx.db)
+    .await?;
+
+    let client = Client::new();
+
+    let response = client
+        .get(format!(
+            "https://sandbox.cashfree.com/pg/orders/{}",
+            cf_order_id
+        ))
+        .header("x-api-version", "2023-08-01")
+        .header("Accept", "application/json")
+        .send()
+        .await
+        .context("failed to verify payment")?;
+
+    if !response.status().is_success() {
+        return Err(anyhow::anyhow!("failed to verify payment").into());
+    }
+
+    let response: PaymentStatus = response.json().await.context("failed to parse response")?;
+
+    if response.order_status == "PENDING" {
+        return Ok("PENDING");
+    }
+
+    if response.order_status == "PAID" {
+        sqlx::query!(
+            r#"update "order" set status = 'paid' where order_id = $1"#,
+            order_id
+        )
+        .execute(&ctx.db)
+        .await?;
+
+        sqlx::query!(
+            r#"update "payment" set status = 'paid' where order_id = $1"#,
+            order_id
+        )
+        .execute(&ctx.db)
+        .await?;
+
+        return Ok("PAID");
+    } else {
+        sqlx::query!(
+            r#"update "order" set status = 'failed' where order_id = $1"#,
+            order_id
+        )
+        .execute(&ctx.db)
+        .await?;
+        sqlx::query!(
+            r#"update "payment" set status = 'failed' where order_id = $1"#,
+            order_id
+        )
+        .execute(&ctx.db)
+        .await?;
+
+        return Ok("FAILED");
+    }
+
+    Ok("")
 }
 
 async fn get_pending_orders(auth: Auth, ctx: State<AppContext>) -> Result<Json<Vec<Order>>> {
